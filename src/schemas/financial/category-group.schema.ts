623f5804376e2c086fd11267913541e21ac38757
Julio Cesar Sulato Filho <31974724+mazarotto-dev@users.noreply.github.com>
2025-09-05 22:09:28 -0300

    Include modals in string

--- src/schemas/financial/category-group.schema.ts
+++ src/schemas/financial/category-group.schema.ts
@@ -2,7 +2,7 @@ import toTitleCase from '@/utils/toTitleCase';
 import { z } from 'zod';
 
 export const getCreateCategoryGroupSchema = (t: (key: string) => string) => z.object({
-  name: z.string().min(1, { message: t('fields.name.errors.required') }).transform(toTitleCase),
+  name: z.string().min(1, { message: t('modals.fields.name.errors.required') }).transform(toTitleCase),
 });
 
 export const getUpdateCategoryGroupSchema = (t: (key: string) => string) => getCreateCategoryGroupSchema(t).partial();

77eecd353b6235e83f3a0da90bbc4a2f319cf9a2
Julio Cesar Sulato Filho <31974724+mazarotto-dev@users.noreply.github.com>
2026-01-07 14:15:41 -0300

    utils: add form data parser and validation error handler

---
+++ src/utils/handleValidationErrors.ts
@@ -0,0 +1,10 @@
+import { ActionState } from "@/types/server-actions.types";
+import z from "zod";
+
+export function handleValidationErrors<T>(error: z.ZodError): ActionState<T> {
+  const flattenedErrors = z.flattenError(error);
+  return {
+    errors: flattenedErrors.fieldErrors,
+    success: false,
+  };
+}

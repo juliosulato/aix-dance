9974044f45828801b2be9db81b319d3ee2d91598
Julio Cesar Sulato Filho <31974724+mazarotto-dev@users.noreply.github.com>
2026-01-25 19:11:22 -0300

    feat(schema): refactor class schema and remove deprecated class.schema.ts

--- src/schemas/academic/class.schema.ts
+++ src/schemas/academic/class.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-enum DAYS_OF_WEEK {
+export enum DAYS_OF_WEEK {
     SUNDAY = "SUNDAY",
     MONDAY = "MONDAY",
     TUESDAY = "TUESDAY",
@@ -11,33 +11,24 @@ enum DAYS_OF_WEEK {
 }
 
 const classSchema = z.object({
-    name: z.string("Esperado um valor do tipo string")
-        .min(1, "Nome da turma é obrigatório"),
-    modalityId: z.string("Esperado um valor do tipo string")
-        .min(1, "ID da modalidade é obrigatório"),
-    teacherId: z.string("Esperado um valor do tipo string")
-        .min(1, "ID do professor é obrigatório"),
-    assistantId: z.string("Esperado um valor do tipo string")
-        .optional(),
-    online: z.boolean("Esperado um valor do tipo boolean")
-        .optional()
-        .default(false),
+    name: z.string().min(1, "Nome da turma é obrigatório"),
+    modalityId: z.string().min(1, "ID da modalidade é obrigatório"),
+    teacherId: z.string().min(1, "ID do professor é obrigatório"),
+    assistantId: z.string().optional(),
+    online: z.boolean().optional().default(false),
     days: z.array(z.object({
-        dayOfWeek: z.enum(DAYS_OF_WEEK, "O dia da semana deve ser válido."),
-        initialHour: z.string("Esperado um valor do tipo string")
-            .min(1, "Horário inicial é obrigatório"),
-        endHour: z.string("Esperado um valor do tipo string")
-            .min(1, "Horário final é obrigatório"),
-    }))
-        .min(1, "É necessário fornecer pelo menos um dia de aula"),
-    students: z.array(
-        z.string("Esperado um valor do tipo string")
-    )
-        .optional()
+        dayOfWeek: z.enum(DAYS_OF_WEEK, "Dia da semana é obrigatório"),
+        initialHour: z.string().min(1, "Horário inicial é obrigatório"),
+        endHour: z.string().min(1, "Horário final é obrigatório"),
+    })).min(1, "É necessário fornecer pelo menos um dia de aula"),
+    students: z.array(z.string()).optional(),
 });
 
 export const createClassSchema = classSchema;
-export const updateClassSchema = classSchema.partial().extend({ id: z.string() }).refine((data) => Object.keys(data).length > 1, {
+
+export const updateClassSchema = classSchema.partial().extend({ 
+    id: z.string() 
+}).refine((data) => Object.keys(data).length > 1, {
     message: "Pelo menos um campo além do ID deve ser fornecido para atualização.",
 });
 
